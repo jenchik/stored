166eee10e3f03c2e11c7410dfdf42e7a7ca40571
Evgenii <power_off@rambler.ru>
2015-10-27 17:32:50 +0300

    fix; added safe mode for multimap

---
+++ multimap/item.go
@@ -0,0 +1,112 @@
+package multimap
+
+import (
+	"github.com/jenchik/stored/api"
+)
+
+type mapItem struct {
+	sm    *mMap
+	key   string
+	stop  bool
+	child api.Mapper
+	inst  int
+}
+
+var _ api.Mapper = &mapItem{}
+
+func newMapper(sm *mMap) *mapItem {
+	return &mapItem{
+		sm:   sm,
+		inst: -1,
+	}
+}
+
+func (m *mapItem) do(f func(api.StoredMap) bool) {
+	for i, _ := range m.sm.maps {
+		if i == m.inst {
+			continue
+		}
+		if f(m.sm.maps[i]) {
+			return
+		}
+	}
+}
+
+func find(m *mapItem, key string) (value interface{}, found bool) {
+	if m.child != nil {
+		if value, found = m.child.Find(key); found {
+			return
+		}
+	}
+	m.do(func(cm api.StoredMap) bool {
+		value, found = cm.Find(key)
+		return found
+	})
+	return
+}
+
+func (m *mapItem) Find(key string) (value interface{}, found bool) {
+	return find(m, key)
+}
+
+func (m *mapItem) Key() string {
+	return m.key
+}
+
+func (m *mapItem) SetKey(key string) {
+	m.key = key
+}
+
+func (m *mapItem) Value() interface{} {
+	v, _ := find(m, m.key)
+	return v
+}
+
+func (m *mapItem) Delete() {
+	if m.child != nil {
+		m.child.Delete()
+		return
+	}
+	m.do(func(cm api.StoredMap) bool {
+		cm.Delete(m.key)
+		return false
+	})
+}
+
+func (m *mapItem) Update(value interface{}) {
+	if m.child != nil {
+		m.child.Update(value)
+	}
+	m.do(func(cm api.StoredMap) bool {
+		cm.Insert(m.key, value)
+		return false
+	})
+}
+
+func (m *mapItem) Len() int {
+	// TODO
+	if m.child == nil {
+		return m.sm.Len()
+	}
+	return m.child.Len()
+}
+
+func (m *mapItem) Lock() {
+	// TODO
+}
+
+func (m *mapItem) Unlock() {
+	// TODO
+}
+
+func (m *mapItem) Stop() {
+	m.stop = true
+}
+
+func (m *mapItem) Clear() {
+	// TODO
+}
+
+func (m *mapItem) Close() {
+	// TODO
+}
---
+++ multimap/item_cache.go
@@ -0,0 +1,87 @@
+package multimap
+
+import (
+	"github.com/jenchik/stored/api"
+)
+
+type mapItemCache struct {
+	sm    *mMap
+	key   string
+	stop  bool
+	cache map[string]interface{}
+}
+
+var _ api.Mapper = &mapItemCache{}
+
+func newMapperCache(sm *mMap) *mapItemCache {
+	return &mapItemCache{
+		sm:    sm,
+		cache: make(map[string]interface{}, sm.Len()),
+	}
+}
+
+func (m *mapItemCache) find(key string) (value interface{}, found bool) {
+	value, found = m.cache[key]
+	return
+}
+
+func (m *mapItemCache) do(f func(api.StoredMap) bool) {
+	for i, _ := range m.sm.maps {
+		f(m.sm.maps[i])
+	}
+}
+
+func (m *mapItemCache) Find(key string) (value interface{}, found bool) {
+	return m.find(key)
+}
+
+func (m *mapItemCache) Key() string {
+	return m.key
+}
+
+func (m *mapItemCache) SetKey(key string) {
+	m.key = key
+}
+
+func (m *mapItemCache) Value() interface{} {
+	v, _ := m.find(m.key)
+	return v
+}
+
+func (m *mapItemCache) Delete() {
+	m.do(func(cm api.StoredMap) bool {
+		cm.Delete(m.key)
+		return false
+	})
+}
+
+func (m *mapItemCache) Update(value interface{}) {
+	m.do(func(cm api.StoredMap) bool {
+		cm.Insert(m.key, value)
+		return false
+	})
+}
+
+func (m *mapItemCache) Len() int {
+	return len(m.cache)
+}
+
+func (m *mapItemCache) Lock() {
+	// TODO
+}
+
+func (m *mapItemCache) Unlock() {
+	// TODO
+}
+
+func (m *mapItemCache) Stop() {
+	m.stop = true
+}
+
+func (m *mapItemCache) Clear() {
+	// TODO
+}
+
+func (m *mapItemCache) Close() {
+	// TODO
+}
